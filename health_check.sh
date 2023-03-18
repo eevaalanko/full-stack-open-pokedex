d5c64ed315c9153d48f6a7f05c67b3b16939cbdf
eeva.alanko <eeva.alanko@siili.com>
2023-03-18 18:52:59 +0200

    testing MMMMMM

--- health_check.sh
+++ health_check.sh
@@ -3,5 +3,5 @@
 echo "Hello from shell script"
 exit 0
 
-# curl http://localhost:8080 -k -s -f -o /dev/null && echo "SUCCESS" && exit 0 || echo "ERROR" && exit 1
+curl http://localhost:8080 -k -s -f -o /dev/null && echo "SUCCESS" && exit 0 || echo "ERROR" && exit 1
 
