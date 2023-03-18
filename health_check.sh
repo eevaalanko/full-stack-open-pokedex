dba2feb73c9e068e670b43d069680b8e115efdb0
eeva.alanko <eeva.alanko@siili.com>
2023-03-18 17:02:57 +0200

    testing

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Hello from shell script"
+exit 0
 
-curl http://localhost:8080 -k -s -f -o /dev/null && echo "SUCCESS" && exit 0 || echo "ERROR" && exit 1
+# curl http://localhost:8080 -k -s -f -o /dev/null && echo "SUCCESS" && exit 0 || echo "ERROR" && exit 1
 
